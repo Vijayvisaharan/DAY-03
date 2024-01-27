4bf60b9837045b25f7a5836f67378c35b44c6c5a
vijayvisaharan <vijayvisaharan@gmail.com>
2024-01-27 19:51:08 +0530

    DAY 03

--- script 1.js
+++ script 1.js
@@ -1,6 +1,19 @@
+
 var obj1={name:"Person 1",age:5};
 var obj2={age:5,name:"Person 1"};
-return JSON.stringify(Obj1) === JSON.stringify(Obj2);
+
+if (obj1.length !== obj2.length){
+  console.log("false")
+}
+else {
+  // Comparing each element of your array
+  for (var i = 0; i < obj1.length; i++) {
+    if (obj1[i] !== obj2[i]) {
+     console.log("false")
+    }
+  }
+  console.log("true")
+}
 
 
 
